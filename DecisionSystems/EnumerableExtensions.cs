50a380bb10913751a01ad87205052146b5d77d78
Sebastian.WALDHOER <bier.bierschtla@prost.at>
2020-10-05 16:44:12 +0200

    Rewrite GetDistance using Linq

---
+++ DecisionSystems/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecisionSystems
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TOut> Pairwise<TIn, TOut>(this IEnumerable<TIn> items, Func<TIn, TIn, TOut> merge)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+            return items.Zip(items.Skip(1), merge);
+        }
+    }
+}
--- DecisionSystems/TSP/Utils.cs
+++ DecisionSystems/TSP/Utils.cs
@@ -11,22 +11,44 @@ namespace DecisionSystems.TSP
     {
         public static double GetDistance(IReadOnlyCollection<int> solution, IReadOnlyList<Location> cities)
         {
-            var distance = 0.0;
-            Location help = new Location(0,0);
-            help = cities[solution.Last()-1];                
-            
-            foreach (int index in solution)
+            //var distance = 0.0;
+            //Location help = new Location(0,0);
+            //help = cities[solution.Last()-1];                
+
+            //foreach (int index in solution)
+            //{
+            //    distance +=GetDistance(help, cities[index-1]);
+            //    help = cities[index-1];
+            //}
+
+            //return distance;
+
+            Location getLocation(int solutionIndex)
             {
-                distance +=GetDistance(help, cities[index-1]);
-                help = cities[index-1];
+                return cities[solutionIndex - 1];
             }
-           
-            return distance;
+
+            return solution
+                .Append(solution.First())
+                .Select(getLocation)
+                .Pairwise(GetDistance)
+                .Sum();
         }
 
         private static double GetDistance(Location help, Location location)
         {
+            if (help is null)
+            {
+                throw new ArgumentNullException(nameof(help));
+            }
+
+            if (location is null)
+            {
+                throw new ArgumentNullException(nameof(location));
+            }
+
             return System.Math.Sqrt(Math.Pow((location.X - help.X),2.00) + Math.Pow((location.Y - help.Y), 2.00));
         }
+       
     }
 }
