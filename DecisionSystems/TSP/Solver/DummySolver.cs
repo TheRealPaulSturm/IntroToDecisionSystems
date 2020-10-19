785b7cae6faa06aa1d065fb4898941076e3d1781
Sebastian.WALDHOER <bier.bierschtla@prost.at>
2020-10-19 16:42:29 +0200

    new solver

---
+++ DecisionSystems/TSP/Solver/DummySolver.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+
+namespace DecisionSystems.TSP.Solver
+{
+    public class DummySolver : ITSPSolver
+    {
+        public List<int> Solve(IReadOnlyList<Location> cities)
+        {
+            var output = new List<int>(cities.Count);
+
+            for (int i = 1; i <= cities.Count; i++)
+            {
+                output.Add(i);
+            }
+            return output;
+        
+        }
+    }
+}
